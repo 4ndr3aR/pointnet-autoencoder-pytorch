824d57383fad4d0ffd51e97dc928e3b85032a1a3
Andrea Ranieri <andrea.ranieri@cnr.it>
2024-05-28 12:08:55 +0200

    Renamed "dataprep" in a more canonic "dataset" and made the whole machinery work (on cpu only for now...)

--- train_shapenet.py
+++ train_shapenet.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import torch 
 import argparse
 import os
@@ -5,44 +7,75 @@ import os
 from torch.utils.data import DataLoader
 from torch import optim
 
-from dataprep.dataset import PointCloudDataset
+#from dataset.dataset import PointCloudDataset
+#from dataset.shapenetdataset import ShapeNetDataset
+from dataset.SymmetryNet import SymmetryNet
+
 from model.model import PCAutoEncoder
 from chamfer_distance.chamfer_distance_cpu import ChamferDistance # https://github.com/chrdiller/pyTorchChamferDistance
 
+from dataset.transforms.ComposeTransform import ComposeTransform
+from dataset.transforms.RandomSampler import RandomSampler
+from dataset.transforms.UnitSphereNormalization import UnitSphereNormalization
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument("--batch_size", type=int, default=32, help="input batch size")
-parser.add_argument("--num_points", type=int, required=True, help="Number of Points to sample")
-parser.add_argument("--num_workers", type=int, default=4, help="Number Multiprocessing Workers")
-parser.add_argument("--dataset_path", required=True, help="Path to Dataset")
-parser.add_argument("--nepoch", type=int, required=True, help="Number of Epochs to train for")
+parser.add_argument("--batch_size"  , type=int, default=32    , help="input batch size")
+parser.add_argument("--num_points"  , type=int, default=10000 , help="Number of Points to sample")
+parser.add_argument("--num_workers" , type=int, default=1     , help="Number Multiprocessing Workers")
+parser.add_argument("--dataset_path", type=str, default='/tmp', help="Path to Dataset")
+parser.add_argument("--nepoch"      , type=int, default=500   , help="Number of Epochs to train for")
 
-ip_options = parser.parse_args()
-print(f"Input Arguments : {ip_options}")
+args = parser.parse_args()
+print(f"Input Arguments : {args}")
+
+num_points = args.num_points
+point_dim  = 3
 
 # Creating Dataset
-# train_ds = PointCloudDataset(ip_options.dataset_path, ip_options.num_points, 'train')
-# test_ds = PointCloudDataset(ip_options.dataset_path, ip_options.num_points, 'test')
+# train_ds = PointCloudDataset(args.dataset_path, args.num_points, 'train')
+# test_ds = PointCloudDataset(args.dataset_path, args.num_points, 'test')
 
-from dataprep.shapenetdataset import ShapeNetDataset
-train_ds = ShapeNetDataset(ip_options.dataset_path, ip_options.num_points, split='train')
-test_ds = ShapeNetDataset(ip_options.dataset_path, ip_options.num_points, split='test')
+scaler  = UnitSphereNormalization()
+sampler = RandomSampler(sample_size=num_points, keep_copy=True)
+compose_transform = ComposeTransform([sampler, scaler])
 
+
+'''
+train_ds = ShapeNetDataset(args.dataset_path, args.num_points, split='train')
+test_ds  = ShapeNetDataset(args.dataset_path, args.num_points, split='test')
+'''
+sym_ds   = SymmetryNet(args.dataset_path, compose_transform)
+train_ds = sym_ds.train_set
+test_ds  = sym_ds.valid_set
+sym_ds.train()
+
+def collate_fn(data, debug=False):
+	pcd_lst, lbl_lst = zip(*data)
+	if debug:
+		print(f'{pcd_lst = }')
+		print(f'{lbl_lst = }')
+	pcdt = torch.stack(pcd_lst)
+	lblt = torch.stack(lbl_lst)
+	if debug:
+		print(f'{pcdt.shape}\n{pcdt = }')
+		print(f'{lblt.shape}\n{lblt = }')
+	return pcdt, lblt
 
 # Creating DataLoader 
-train_dl = DataLoader(test_ds, batch_size=ip_options.batch_size, shuffle=True, num_workers= ip_options.num_workers)
-train_dl = DataLoader(train_ds, batch_size=ip_options.batch_size, shuffle=True, num_workers= ip_options.num_workers)
+#train_dl = DataLoader(train_ds, batch_size=args.batch_size, shuffle=True, num_workers=args.num_workers)
+#test_dl  = DataLoader(test_ds,  batch_size=args.batch_size, shuffle=True, num_workers=args.num_workers)
+train_dl = DataLoader(sym_ds, batch_size=args.batch_size, shuffle=True, num_workers=args.num_workers, collate_fn=collate_fn)
+
 
 # Output of the dataloader is a tensor reprsenting
 # [batch_size, num_channels, height, width]
 
 # getting one data sample
-sample = next(iter(train_ds))
+sample = next(iter(train_dl))
+print(f'{sample[0].shape = } - {sample[1]}\n{sample[0] = }')
 
 # Creating Model
-num_points = ip_options.num_points
-point_dim = 3
 
 autoencoder = PCAutoEncoder(point_dim, num_points)
 
@@ -62,9 +95,9 @@ os.makedirs('saved_models', exist_ok=True)
 chamfer_dist = ChamferDistance()
 
 # Start the Training 
-for epoch in range(ip_options.nepoch):
+for epoch in range(args.nepoch):
     for i, data in enumerate(train_dl):
-        points = data
+        points, labels = data
         points = points.transpose(2, 1)
         # points = points.cuda()
         optimizer.zero_grad()
@@ -82,10 +115,3 @@ for epoch in range(ip_options.nepoch):
 
     scheduler.step()
     torch.save(autoencoder.state_dict(), 'saved_models/autoencoder_%d.pth' % (epoch))
-        
-
-
-        
-
-
-
